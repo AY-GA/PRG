c37b1c217e85f204cedd7450b2e2a394ab86dd03
AY-GA <149565612+AY-GA@users.noreply.github.com>
2025-05-23 12:06:45 +0200

    Add files via upload

--- 2425_GA_2E/src/lekce_22/PolozkaSeznamu.java
+++ 2425_GA_2E/src/lekce_22/PolozkaSeznamu.java
@@ -1,8 +1,8 @@
 package lekce_22;
 
 public class PolozkaSeznamu {
-    int hodnota;
-    PolozkaSeznamu dalsi;
+    public int hodnota;
+    public PolozkaSeznamu dalsi;
 
     public PolozkaSeznamu(int hodnota, PolozkaSeznamu dalsi) {
         this.hodnota = hodnota;
