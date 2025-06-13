17839ef78b44ad0652ab9fffe2e4293b3a4e8750
AY-GA <149565612+AY-GA@users.noreply.github.com>
2025-06-13 12:12:48 +0200

    Delete 2425_GA_2E directory

--- 2425_GA_2E/src/lekce_03/Pes.java
+++
@@ -1,7 +0,0 @@
-package lekce_03;
-
-public class Pes extends Zvire {
-    public String delaZvuk() {
-        return "Haf";
-    }
-}
--- 2425_GA_2E/src/lekce_12/Clovek.java
+++
@@ -1,7 +0,0 @@
-package lekce_12;
-
-public class Clovek {
-    String jmeno;
-    String prijmeni;
-
-}
