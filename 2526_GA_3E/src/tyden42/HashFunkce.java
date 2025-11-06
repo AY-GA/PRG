e07720c38596f9973d8fb000e76748c67c5c3066
AY23 <149565612+AY-GA@users.noreply.github.com>
2025-11-06 09:21:53 +0100

    Add files via upload

--- 2526_GA_3E/src/tyden42/HashFunkce.java
+++ 2526_GA_3E/src/tyden42/HashFunkce.java
@@ -1,15 +1,31 @@
 package tyden42;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class HashFunkce {
+    static String abc = "AÁBCČDĎEÉĚFGHChIÍJKLMNŇOÓPQRŘSŠTŤUÚŮVWXYÝZŽ";
     static void main() {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        scanner.close();
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.nextLine();
+//        scanner.close();
+        while (true) {
+            String input = makeWord();
+            long hash = getHash(input);
+            if (hash == 221) System.out.println(input);
+//            System.out.println("Hash: " + hash);
+        }
+    }
 
-        long hash = getHash(input);
-        System.out.println("Hash: " + hash);
+    private static String makeWord() {
+        int wordSize = 3;
+        StringBuilder str = new StringBuilder();
+        Random random = new Random();
+        for (int i = 0; i < wordSize; i++) {
+            int index = random.nextInt(abc.length());
+            str.append(abc.charAt(index));
+        }
+        return str.toString();
     }
 
     private static long getHash(String msg) {
---
+++ 2526_GA_3E/src/tyden45/GetCert.java
@@ -0,0 +1,23 @@
+package tyden45;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.cert.Certificate;
+import java.util.Arrays;
+
+public class GetCert {
+    static final String url = "https://google.eu";
+
+    static void main() throws IOException {
+        URL destinationURL = new URL(url);
+        HttpsURLConnection conn = (HttpsURLConnection) destinationURL.openConnection();
+        conn.connect();
+        Certificate[] certs = conn.getServerCertificates();
+
+        System.out.println("# of certificates for " + url + ": " + certs.length);
+        System.out.println("\n---------------------------------------------------------------------\n");
+        System.out.println(Arrays.stream(certs).toList());
+    }
+}
